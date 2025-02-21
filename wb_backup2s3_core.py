41d2f5b27dc3b61acf622dd4c0c2da2b08ca9990
Sergey <s_korneev@outlook.com>
2025-02-21 15:07:48 +0200

    Update wb_backup2s3_core.py

--- wb_backup2s3_core.py
+++ wb_backup2s3_core.py
@@ -5,6 +5,9 @@ import botocore
 import datetime
 import json
 import sentry_sdk
+import requests
+import functools
+import re
 from dataclasses import dataclass
 from urllib import parse
 import tableauserverclient as TSC
@@ -13,7 +16,8 @@ from queue import SimpleQueue
 from tableauserverclient.models.workbook_item import WorkbookItem
 from sentry_sdk import add_breadcrumb
 from sentry_sdk.scrubber import DEFAULT_DENYLIST
-from botocore.client import Config
+
+logger = logging.getLogger('main.' + __name__)
 
 SENTRY_DENYLIST = DEFAULT_DENYLIST + [
     'access_key',
@@ -23,12 +27,46 @@ SENTRY_DENYLIST = DEFAULT_DENYLIST + [
     'tableau_cred',
 ]
 
+
 @dataclass
 class Workbook:
     name: str
     id: str
     project: str
     size: int
+    site: str
+
+
+def retry(_func=None, *, times=6):
+    def decorator_retry(func):
+        @functools.wraps(func)
+        def wrapper_retry(*args, **kwargs):
+            last_exception = None
+            for attempt in range(times):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    last_exception = e
+                    logger.debug(f"  Attempt {attempt + 1} failed: {e}. Retrying...")
+            logger.debug("  Function failed after maximum retry attempts.")
+            raise last_exception
+        return wrapper_retry
+    if _func is None:
+        return decorator_retry
+    else:
+        return decorator_retry(_func)
+
+
+def print_and_send_exceptions_senrty(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.exception(e)
+            sentry_sdk.capture_exception(e)
+            raise
+    return wrapper
 
 
 class BackupWB2S3:
@@ -48,25 +86,26 @@ class BackupWB2S3:
     work_dir: str
         Folder where script downloads files before uploading to AWS S3.
 
-
     """
     loger_name = 'main.BackupWB2S3'
     s3_upload_state_file = 'upload_state.json'
     _time_format = '%Y-%m-%d %H:%M:%S%z'
     _date_format = '%Y-%m-%d'
+    _download_error_ignore_tag = 'WBBackupIgnoreErrors'
 
     def __init__(
             self,
             tableau_cred: tuple,
             s3_creds: tuple,
             work_dir: str,
-            exeptions_queue: SimpleQueue = None,
-            result_queue: SimpleQueue = None,
+            failed_q: SimpleQueue = None,
+            successful_q: SimpleQueue = None,
+            ts_http_timeout: int = 1200,
     ):
 
         self.logger = logging.getLogger(self.loger_name)
-        self.exeptions_queue = exeptions_queue if exeptions_queue else SimpleQueue()
-        self.result_queue = result_queue if result_queue else SimpleQueue()
+        self.failed_q = failed_q if failed_q else SimpleQueue()
+        self.successful_q = successful_q if successful_q else SimpleQueue()
         self.work_dir = work_dir
         self.current_site_name = None
         self.project_id_path = None
@@ -88,6 +127,7 @@ class BackupWB2S3:
             server_address=tab_url,
             use_server_version=True
         )
+        self.ts.http_options['timeout'] = ts_http_timeout
 
         self.ts.auth.sign_in(
             TSC.TableauAuth(
@@ -101,7 +141,6 @@ class BackupWB2S3:
             aws_access_key_id=key_id,
             aws_secret_access_key=access_key,
         )
-
         self._fill_user_id_username()
 
     def _get_wb_path(self, wb: WorkbookItem):
@@ -148,105 +187,114 @@ class BackupWB2S3:
     def _ts_get_all_workbooks(self):
         return list(TSC.Pager(self.ts.workbooks))
 
-    def _ts_download_wb(self, wb: WorkbookItem):
+    @retry
+    def _ts_download_wb(self, wb: WorkbookItem, include_extract: bool = True):
         file_path = os.path.join(self.work_dir, wb.id)
-        self.logger.debug(f'Download wb:{wb.project_name} / {wb.name} ({wb.id})')
-
-        file_path = self.ts.workbooks.download(
+        self.logger.info(f' download:{wb.project_name} / {wb.name} ({wb.id})')
+        return self.ts.workbooks.download(
             workbook_id=wb.id,
-            include_extract=True,
+            include_extract=include_extract,
             filepath=file_path,
         )
-        return file_path
 
-    def _backup_wb2s3(self, wb: WorkbookItem):
+    @print_and_send_exceptions_senrty
+    def _do_backup(self, wb: WorkbookItem, include_extract: bool = True):
         wb_path = self._get_wb_path(wb)
+
+        try:
+            file_path = self._ts_download_wb(
+                wb=wb,
+                include_extract=include_extract
+            )
+        except Exception as e:
+            self.logger.warning(f'{wb.name} download failed. Try with include_extract=False')
+            include_extract = False
+            file_path = self._ts_download_wb(
+                wb=wb,
+                include_extract=include_extract
+            )
+
+        obj_key = wb_path + '.' + file_path[-7:].split('.')[1]
+        tags = {
+            'tab_owner': self.user_id_username.get(wb.owner_id),
+            'tab_id': wb.id,
+            'tab_created_at': wb.created_at.strftime(self._time_format),
+            'tab_updated_at': wb.updated_at.strftime(self._time_format),
+            'tab_description': self.convert_to_s3_compliant_tag(wb.description)[:256] if wb.description else '',
+        }
+        self._s3_upload(
+            file_path=file_path,
+            object_key=obj_key,
+            tags=tags
+        )
+        if include_extract or self._download_error_ignore_tag in wb.tags:
+            self.upload_state[wb_path] = {
+                'id': wb.id,
+                'name': wb.name,
+                'created_at': wb.created_at.strftime(self._time_format),
+                'updated_at': wb.updated_at.strftime(self._time_format),
+                'upload_date': datetime.date.today().strftime(self._date_format),
+                'object_key': obj_key,
+            }
+        os.remove(file_path)
+
+    def _backup_wb(self, wb: WorkbookItem):
         workbook = Workbook(
             name=wb.name,
             project=wb.project_name,
             id=wb.id,
-            size=wb.size
+            size=wb.size,
+            site=self.current_site_name,
         )
-        self.logger.info(f'Backup "{wb_path}({wb.id})"')
+        self.logger.info(f'Backup "{wb.project_name}"/"{wb.name}" ({wb.id}), {wb.size} MB"')
         with sentry_sdk.new_scope() as scope:
             scope.add_breadcrumb(
-                category='_backup_wb2s3',
-                message=f'Backup wb:{wb.project_name} / {wb.name} ({wb.id})',
+                category='_backup',
+                message=f'Backup(project / wb (id)): {wb.project_name} / {wb.name} ({wb.id})',
                 level='info',
                 type='debug',
             )
             try:
-                file_path = self._ts_download_wb(wb)
+                self._do_backup(wb)
             except Exception as e:
-                self.logger.warning(f'Error while downloading  "{wb.name}({wb.id})"')
-                self.logger.exception(e)
-                self.exeptions_queue.put((e, workbook))
-                sentry_sdk.capture_exception(e)
+                self.failed_q.put((e, workbook))
             else:
-                obj_key = wb_path + '.' + file_path[-7:].split('.')[1]
-                tags = {
-                    'tab_owner': self.user_id_username.get(wb.owner_id),
-                    'tab_id': wb.id,
-                    'tab_created_at': wb.created_at.strftime(self._time_format),
-                    'tab_updated_at': wb.updated_at.strftime(self._time_format),
-                }
-                try:
-                    self._s3_upload(
-                        file_path=file_path,
-                        object_key=obj_key,
-                        tags=tags
-                    )
-                except Exception as e:
-                    self.logger.warning(f'Error while uploading {obj_key}')
-                    self.logger.exception(e)
-                    self.exeptions_queue.put((e, workbook))
-                    sentry_sdk.capture_exception(e)
-                else:
-                    self.upload_state[wb_path] = {
-                        'id': wb.id,
-                        'name': wb.name,
-                        'created_at': wb.created_at.strftime(self._time_format),
-                        'updated_at': wb.updated_at.strftime(self._time_format),
-                        'upload_date': datetime.date.today().strftime(self._date_format),
-                        'object_key': obj_key,
-                    }
-                    self.result_queue.put(workbook)
-                os.remove(file_path)
+                self.successful_q.put(workbook)
 
     def run_backup(
             self,
             site_names: list = None,
-            last_modified_update_interval: int = 30,
-            threads=False,
-            max_workers: int = 10
+            last_modified_update_interval: int = 60,
+            max_workers: int = 10,
+            excluded_sites: list = []
     ):
-        backup_wb_count = 0
-        wb_sites = self._ts_get_all_sites()
-        if site_names:
-            wb_sites = [i for i in wb_sites if i.name in site_names]
+        ts_sites = [s for s in self._ts_get_all_sites() if s.name not in excluded_sites]
 
-        for site in wb_sites:
-            backup_wb_count += self.backup_site(
+        if site_names:
+            ts_sites = [i for i in ts_sites if i.name in site_names]
+
+        for site in ts_sites:
+            self.backup_site(
                 site_name=site.name,
-                threads=threads,
-                max_workers=max_workers
+                max_workers=max_workers,
+                last_modified_update_interval=last_modified_update_interval,
             )
-            self.s3_update_outdated_last_modified_in_curr_site(last_modified_update_interval)
-        return backup_wb_count
+        return
 
     def backup_site(
             self,
             site_name: str,
-            threads=False,
-            max_workers: int = 10
+            max_workers: int,
+            last_modified_update_interval: int,
     ):
-        self.logger.info(f'Backup site: "{site_name}"')
-        queue_to_backup = []
+        self.logger.info(f'#Backup site: "{site_name}"')
         self._ts_switch_site(site_name)
 
-        all_wbs = self._ts_get_all_workbooks()
+        queue_to_backup = []
 
+        all_wbs = self._ts_get_all_workbooks()
         all_wbs_paths = [self._get_wb_path(w) for w in all_wbs]
+
         for wb_path, wb_data in [(k, v) for k, v in self.upload_state.items() if k not in all_wbs_paths]:
             self.logger.info(f'"{wb_data['object_key']}" no longer exists on the TS. Update Last modified field in S3')
             self._s3_update_last_modified(wb_data['object_key'])
@@ -254,6 +302,7 @@ class BackupWB2S3:
 
         for wb in all_wbs:
             wb_path = self._get_wb_path(wb)
+
             if self.upload_state.get(wb_path) and all([
                 self.upload_state[wb_path]['id'] == wb.id,
                 self.upload_state[wb_path]['updated_at'] == wb.updated_at.strftime(self._time_format),
@@ -261,14 +310,30 @@ class BackupWB2S3:
             ]):
                 self.logger.debug(f'"{wb_path}" already in S3 and has the same metadata. Ignore')
             else:
+                if self.upload_state.get(wb_path):
+                    msg_parts = []
+                    if self.upload_state[wb_path]['id'] != wb.id:
+                        msg_parts.append(f'wb id was changed: {self.upload_state[wb_path]['id']} -> {wb.id}')
+                    elif self.upload_state[wb_path]['updated_at'] != wb.updated_at.strftime(self._time_format):
+                        msg_parts.append(
+                            f'wb updated_at was changed: {self.upload_state[wb_path]['updated_at']} -> {wb.updated_at.strftime(self._time_format)}'
+                        )
+                    elif self.upload_state[wb_path]['created_at'] == wb.created_at.strftime(self._time_format):
+                        msg_parts.append(
+                            f'wb created_at was changed: {self.upload_state[wb_path]['created_at']} -> {wb.created_at.strftime(self._time_format)}'
+                        )
+                    self.logger.info(f'"{wb_path}": ' + ' ,'.join(msg_parts))
                 queue_to_backup.append(wb)
 
-        if threads:
-            with ThreadPoolExecutor(max_workers=max_workers) as tpe:
-                resp = [tpe.submit(self._backup_wb2s3, wb) for wb in queue_to_backup]
-        else:
-            for wb in queue_to_backup:
-                self._backup_wb2s3(wb)
+        with ThreadPoolExecutor(max_workers=max_workers) as tpe:
+            resp = [tpe.submit(self._backup_wb, wb) for wb in queue_to_backup]
+        for r in resp:
+            if r.exception():
+                self.logger.exception(r.exception())
+                self.failed_q.put((r.exception(), None))
+                sentry_sdk.capture_exception(r.exception())
+
+        self._s3_update_outdated_last_modified(last_modified_update_interval)
         self._s3_upload_upload_state()
         return len(queue_to_backup)
 
@@ -282,6 +347,7 @@ class BackupWB2S3:
                 raise
         return True
 
+    @retry
     def _s3_upload(
             self,
             file_path: str,
@@ -297,7 +363,7 @@ class BackupWB2S3:
             params['ExtraArgs'] = {
                 "Tagging": parse.urlencode(tags)
             }
-        self.logger.debug(f'Start upload: {object_key} to {self.bucket_name}')
+        self.logger.info(f' upload: {object_key} to {self.bucket_name}')
         resp = self.s3.upload_file(**params)
 
     def _s3_list_all_objects_in_curr_ts_site(self):
@@ -309,11 +375,31 @@ class BackupWB2S3:
                 all_objects += [i for i in page['Contents'] if i['Key'].startswith(self.current_site_name + '/')]
         return all_objects
 
-    def s3_update_outdated_last_modified_in_curr_site(self, days: int = 30):
+    @staticmethod
+    def convert_to_s3_compliant_tag(input: str, replacement_char='_'):
+        allowed_pattern = r'[а-яА-Яa-zA-Z0-9 +\-=\.:/@]'
+
+        output = ''.join(
+            char if re.match(allowed_pattern, char) else replacement_char
+            for char in input
+        )
+        return output
+
+    def _s3_update_outdated_last_modified(self, days: int = 30, threads: bool = True, max_workers: int = 10):
+        curr_date = datetime.datetime.now().astimezone()
         all_object = self._s3_list_all_objects_in_curr_ts_site()
-        #ToDo Add Threads
-        for obj in all_object:
-            if (datetime.datetime.now().astimezone() - obj['LastModified']).days >= days:
+
+        if threads:
+            with ThreadPoolExecutor(max_workers=max_workers) as tpe:
+                resp = [tpe.submit(self._s3_update_last_modified, obj['Key']) for obj in
+                        [i for i in all_object if (curr_date - i['LastModified']).days >= days]]
+            for r in resp:
+                if r.exception():
+                    self.logger.exception(r.exception())
+                    self.failed_q.put((r.exception(), None))
+                    sentry_sdk.capture_exception(r.exception())
+        else:
+            for obj in [i for i in all_object if (curr_date - i['LastModified']).days >= days]:
                 self._s3_update_last_modified(obj['Key'])
 
     def _s3_update_last_modified(self, object_key: str):
@@ -323,11 +409,12 @@ class BackupWB2S3:
             CopySource={'Bucket': self.bucket_name, 'Key': object_key},
             Key=object_key
         )
-        self.logger.debug(f'self.s3.copy_object resp: {resp}')
+        # self.logger.debug(f'self.s3.copy_object resp: {resp}')
 
+    @retry(times=3)
     def _s3_upload_upload_state(self):
         obj_key = self.current_site_name + '/' + self.s3_upload_state_file
-        self.logger.info(f'Upload upload_state in {obj_key} ')
+        self.logger.info(f'Upload {obj_key} ')
         with sentry_sdk.new_scope() as scope:
             upload_state = json.dumps(self.upload_state, indent=2)
             scope.add_breadcrumb(
@@ -345,7 +432,7 @@ class BackupWB2S3:
                     Body=upload_state,
                     Bucket=self.bucket_name,
                     Key=obj_key
-            )
+                )
             except Exception as e:
                 sentry_sdk.capture_exception(e)
                 raise
@@ -354,7 +441,7 @@ class BackupWB2S3:
         obj_key = self.current_site_name + '/' + self.s3_upload_state_file
         self.logger.debug(f'Try to download {obj_key} ')
         with sentry_sdk.new_scope() as scope:
-            add_breadcrumb(
+            scope.add_breadcrumb(
                 category='_s3_download_upload_state',
                 message=f'get object "{obj_key}"',
                 level='info',
@@ -366,13 +453,14 @@ class BackupWB2S3:
                     Key=obj_key
                 )
             except  botocore.exceptions.ClientError as e:
-                if e.response.get('Error',{}).get('Code') == 'NoSuchKey':
+                if e.response.get('Error', {}).get('Code') == 'NoSuchKey':
+                    self.logger.warning(obj_key + ' not found. Set upload_state = {}')
                     self.upload_state = {}
                     return
                 sentry_sdk.capture_exception(e)
                 raise
             resp_body = resp["Body"].read()
-            add_breadcrumb(
+            scope.add_breadcrumb(
                 category='_s3_download_upload_state',
                 message='Parse JSON and convert it into dict',
                 level='info',
